0699b943c9b5b66e4629f3da614a47f4a081d50b
Wavyabe <abrahamdharhan2255@gmail.com>
2021-07-14 18:43:02 -0700

    first commit

---
+++ src/components/pages/About.js
@@ -0,0 +1,15 @@
+import React from "react";
+
+function About() {
+  return (
+    <React.Fragment>
+      <h1 style={{ textAlign: "center" }}>About</h1>
+      <p style={{ textAlign: "center" }}>
+        This is a simple TodoList I built as I was learning React and
+        JavaScript.
+      </p>
+    </React.Fragment>
+  );
+}
+
+export default About;
